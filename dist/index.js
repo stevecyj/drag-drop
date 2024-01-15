a1af306ce72483d87c7d0f86643734b018b9a810
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-01-15 17:08:58 +0800

    feat: update form handling in index files
    - Add `id` attribute to the form element in `dist/index.js`
    - Add event listener for form submission in `dist/index.js`
    - Remove unnecessary comments from `src/index.ts`
    - Add `id` attribute to the form element in `src/index.ts`
    - Add event listener for form submission in `src/index.ts`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,25 @@
 "use strict";
 class Project {
-  constructor() {
-    this.templateElem = document.querySelector("#project");
-    this.renderElem = document.querySelector("#app");
-    const imported = document.importNode(this.templateElem.content, true);
-    this.formElem = imported.firstElementChild;
-    this.attach();
-  }
-  attach() {
-    this.renderElem.insertAdjacentElement("afterbegin", this.formElem);
-  }
+    constructor() {
+        this.templateElem = document.querySelector("#project");
+        this.renderElem = document.querySelector("#app");
+        const imported = document.importNode(this.templateElem.content, true);
+        this.formElem = imported.firstElementChild;
+        this.formElem.id = "user-input";
+        this.titleElem = this.formElem.querySelector("#title");
+        this.descElem = (this.formElem.querySelector("#description"));
+        this.peopleElem = this.formElem.querySelector("#people");
+        this.attach();
+        this.config();
+    }
+    attach() {
+        this.renderElem.insertAdjacentElement("afterbegin", this.formElem);
+    }
+    config() {
+        this.formElem.addEventListener("submit", (e) => {
+            e.preventDefault();
+            console.log(this.titleElem.value);
+        });
+    }
 }
 const project = new Project();
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,37 @@ class Project {
   templateElem: HTMLTemplateElement;
   renderElem: HTMLTemplateElement;
   formElem: HTMLTemplateElement;
+  titleElem: HTMLInputElement;
+  descElem: HTMLInputElement;
+  peopleElem: HTMLInputElement;
   constructor() {
     this.templateElem = <HTMLTemplateElement>document.querySelector("#project");
     this.renderElem = <HTMLTemplateElement>document.querySelector("#app");
 
     const imported = document.importNode(this.templateElem.content, true);
     this.formElem = <HTMLTemplateElement>imported.firstElementChild;
+    this.formElem.id = "user-input";
+
+    this.titleElem = <HTMLInputElement>this.formElem.querySelector("#title");
+    this.descElem = <HTMLInputElement>(
+      this.formElem.querySelector("#description")
+    );
+    this.peopleElem = <HTMLInputElement>this.formElem.querySelector("#people");
 
     this.attach();
+
+    this.config();
   }
 
   private attach() {
     this.renderElem.insertAdjacentElement("afterbegin", this.formElem);
+  }
+
+  private config() {
+    this.formElem.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log(this.titleElem.value);
+    });
   }
 }
 
